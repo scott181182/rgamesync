c654f62cbb24113f95bfeb4dd38910c5784fccf8
Scott Fasone <scott181182@yahoo.com>
2025-07-21 23:04:44 -0400

    refactor: updated the temp version script per clippy

--- src/commands/version.rs
+++ src/commands/version.rs
@@ -5,6 +5,6 @@ use rgamesync_rclone::{RCloneError, run_rclone};
 pub fn run_version() -> Result<(), RCloneError> {
     run_rclone(["version"])?;
     // TODO: add macro to get crate version.
-    println!("rgamesync v{}", "0.1.0");
+    println!("rgamesync v0.1.0");
     Ok(())
 }
