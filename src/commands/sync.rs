2ee16e8f0d53f2d4137ab15dc99f9eadd97d915b
Scott Fasone <scott181182@yahoo.com>
2025-07-21 23:04:30 -0400

    feat: added initial sync command (untested)

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,41 +1,69 @@
-use clap::Args;
-use rgamesync_config::GameList;
+use std::mem::swap;
 
-use crate::{context::GameSyncContext, stores::{GameEntry, GameStore, ListGameError}};
+use anyhow::anyhow;
+use clap::Args;
+use rgamesync_config::GameConfig;
+
+use crate::context::GameSyncContext;
+
+
+
+#[derive(Args, Debug)]
+#[group(required = true, multiple = false)]
+pub struct SyncDirectionArg {
+    /// Sync to the remote.
+    /// Mutually exclusive with --from.
+    #[arg(long)]
+    to: bool,
+    /// Sync from the remote.
+    /// Mutually exclusive with --to.
+    #[arg(long)]
+    from: bool,
+}
 
 #[derive(Args, Debug)]
 pub struct SyncArgs {
     /// Sync specific games. If none are specified, sync all games from the config file.
-    games: Vec<String>
+    games: Vec<String>,
+
+    #[command(flatten)]
+    direction: SyncDirectionArg,
 }
 
 
 
-fn get_games_from_list(games: &Vec<String>, list: &GameList, store: &GameStore) -> Result<Vec<GameEntry>, ListGameError> {
-    if !games.is_empty() {
-        store.filter_games(games)
+fn get_games_from_list(games: &[String], list: &[GameConfig]) -> anyhow::Result<Vec<GameConfig>> {
+    if games.is_empty() {
+        Ok(list.to_vec())
     } else {
-        match list {
-            GameList::All => store.list_games(),
-            GameList::List(items) => store.filter_games(items),
-        }
+        let filtered_games = list
+            .iter()
+            .filter(|g| games.contains(&g.name))
+            .map(GameConfig::clone)
+            .collect();
+
+        Ok(filtered_games)
     }
-    
 }
 
-pub fn run_sync(
-    ctx: &GameSyncContext,
-    args: SyncArgs
-) -> anyhow::Result<()> {
-    let stores = ctx.config.stores.iter()
-        .map(|(name, path)| GameStore::new(name.clone(), path.clone()))
-        .collect::<Vec<GameStore>>();
-    let store = &stores[0];
+pub fn run_sync(ctx: &GameSyncContext, args: SyncArgs) -> anyhow::Result<()> {
+    let games = get_games_from_list(&args.games, &ctx.config.games)?;
 
-    let games = get_games_from_list(&args.games, &ctx.config.games, store)?;
     for game in games {
-        // TODO: sync each game.
-        ctx.run_rclone(&["sync"])?;
+        let mut src = game.save_dir.to_str().ok_or(anyhow!("Could not get valid string from source game path"))?.to_owned();
+        let mut dest = ctx.get_remote_path(&game);
+
+        if args.direction.from {
+            // Swap src and dest if we want to sync _from_ the remote instead of _to_.
+            swap(&mut src, &mut dest);
+        }
+
+        let mut args = vec!["sync", &src, &dest];
+        if let Some(ref pattern) = game.save_glob {
+            args.extend_from_slice(&[ "--include", pattern.as_str() ]);
+        }
+
+        ctx.run_rclone(&args)?;
     }
 
     Ok(())
--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,6 @@
 use std::ffi::OsString;
 
-use rgamesync_config::GameSyncConfig;
+use rgamesync_config::{GameConfig, GameSyncConfig};
 use rgamesync_rclone::RCloneError;
 
 
@@ -20,8 +20,21 @@ impl GameSyncContext {
             .opts
             .iter()
             .map(OsString::from)
-            .chain(args.into_iter().map(OsString::from))
+            .chain(args.iter().map(OsString::from))
             .collect::<Vec<_>>();
+
+        #[cfg(debug_assertions)]
+        eprintln!("command: {:?}", &args);
+
         rgamesync_rclone::run_rclone(args)
+    }
+
+    pub fn get_remote_path(&self, game: &GameConfig) -> String {
+        let remote_root = self.config.remote.path
+            .clone()
+            .unwrap_or("rgamesync".into());
+        let remote_path = remote_root.join("games").join(&game.name).join("sync");
+
+        format!("{}:{}", self.config.remote.name, remote_path.to_string_lossy())
     }
 }
